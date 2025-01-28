0d4f6e5f6a6641cc33c0f73caa66677cd9700609
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-28 11:31:37 -0300

    feat(FormDepartamento): add department form component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import FormDepartamento from './components/departamentos/formdepartamento/FormDepartamento'
 
 function App() {
 
   return (
     <>
-     <p>RH System - Team One</p>
+      <FormDepartamento />
     </>
   )
 }
