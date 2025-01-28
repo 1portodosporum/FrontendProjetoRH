3c86ac64300c965072545fa3af9f49af50ccb82f
brartioli <brunaartioli.ba@gmail.com>
2025-01-28 12:05:17 -0300

    add Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+
 
 function App() {
 
   return (
     <>
+     <Navbar/>
+     <div className="min-h-[80vh]"></div>
      <p>RH System - Team One</p>
+     <Footer />
+
     </>
   )
 }
